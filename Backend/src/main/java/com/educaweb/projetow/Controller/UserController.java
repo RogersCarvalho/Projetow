67e9610468e77baff2b696493261c371bc440f35
RogersCarvalho <rogerssud@yahoo.com.br>
2022-11-03 19:10:18 -0300

    Criado UserController

---
+++ Backend/src/main/java/com/educaweb/projetow/Controller/UserController.java
@@ -0,0 +1,28 @@
+package com.educaweb.projetow.Controller;
+
+
+import com.educaweb.projetow.model.entidade.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+       @GetMapping
+       public ResponseEntity<User> findall(){
+
+           var user = new User(1L,"Teste", "teste@yahoo.com.br","(32)99174-0603","1234");
+           return ResponseEntity.ok().body(user);
+       }
+
+
+
+
+
+
+}
