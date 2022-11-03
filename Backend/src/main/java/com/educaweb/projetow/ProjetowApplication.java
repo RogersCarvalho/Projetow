8c4b89f50afacd68b1a1bbf4f3f6e8b924449ac5
RogersCarvalho <rogerssud@yahoo.com.br>
2022-11-03 15:37:46 -0300

    Primeiro commit

---
+++ Backend/src/main/java/com/educaweb/projetow/ProjetowApplication.java
@@ -0,0 +1,13 @@
+package com.educaweb.projetow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/educaweb/projetow/ProjetowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educaweb.projetow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
