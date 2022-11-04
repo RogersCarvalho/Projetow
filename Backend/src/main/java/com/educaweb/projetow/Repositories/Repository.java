fe0be7b10b67d4ba039b712aa9965ee00caf4335
RogersCarvalho <rogerssud@yahoo.com.br>
2022-11-04 10:33:04 -0300

    Criando a classe Repository

---
+++ Backend/src/main/java/com/educaweb/projetow/Repositories/Repository.java
@@ -0,0 +1,13 @@
+package com.educaweb.projetow.Repositories;
+
+import com.educaweb.projetow.model.entidade.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository<User,Long> {
+
+
+
+
+
+
+}
